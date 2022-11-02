b06c799eb0bca28d2e6226487655a20e80bfd072
Dave(.nl) van Hoorn <yo@dave.nl>
2022-11-02 21:08:20 +0100

    remove logger

--- main.py
+++ main.py
@@ -779,13 +779,7 @@ if __name__ == "__main__":
                     "clamp": True
                 }
             },
-            "learning_rate_logger": {
-                "target": "main.LearningRateMonitor",
-                "params": {
-                    "logging_interval": "step",
-                    # "log_momentum": True
-                }
-            },
+
             "cuda_callback": {
                 "target": "main.CUDACallback"
             },

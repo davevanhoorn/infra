9539eed718aa567b0a352a7df15047abaf9ba487
Dave(.nl) van Hoorn <yo@dave.nl>
2022-11-05 16:41:23 +0100

    add base

--- server-step-2_automation.sh
+++ server-step-2_automation.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 pip install omegaconf einops pytorch-lightning==1.6.5 test-tube transformers kornia -e git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers --exists-action b -e git+https://github.com/openai/CLIP.git@main#egg=clip --exists-action b setuptools==59.5.0 pillow==9.0.1 torchmetrics==0.6.0 -e . protobuf==3.20.1 gdown -qq diffusers["training"]==0.3.0 transformers ftfy -qq "ipywidgets>=7,<8" ipywidgets==7.7.1 captionizer==1.0.1
-gdown https://drive.google.com/uc?id=1xcpSr6DjX856jBsR_T-jytAUgXt77scj
 mkdir training_images
 wget -nd -r -P ./training_images -A jpg https://dave.nl/tmp/1.jpg
 wget -nd -r -P ./training_images -A jpg https://dave.nl/tmp/2.jpg
@@ -13,9 +12,9 @@ wget -nd -r -P ./training_images -A jpg https://dave.nl/tmp/7.jpg
 wget -nd -r -P ./training_images -A jpg https://dave.nl/tmp/8.jpg
 wget -nd -r -P ./training_images -A jpg https://dave.nl/tmp/9.jpg
 wget -nd -r -P ./training_images -A jpg https://dave.nl/tmp/10.jpg
-rm -rf training_images/.ipynb_checkpoints
 gdown https://drive.google.com/uc?id=1xcpSr6DjX856jBsR_T-jytAUgXt77scj
-python main.py --reg_data_root "regularization_images/person_ddim" --gpus 0, --class_word "person"
+rm -rf training_images/.ipynb_checkpoints
+python main.py --base "configs/stable-diffusion/v1-finetune_unfrozen.yaml" --reg_data_root "regularization_images/person_ddim" --gpus 0, --class_word "person"
 python scripts/stable_txt2img.py --prompt "firstNameLastName person as a masterpiece portrait painting by John Singer Sargent in the style of Rembrandt ultra-hd high quality portrait with gradients and diffuse glow"
 python scripts/stable_txt2img.py --prompt "Highly detailed painting of a beautiful firstNameLastName person, dancing in the rain, intricate, high quality oil painting artstyle, in the style of anna dittmann, deviantart, figurative art, deviantart, ilya kuvshinov, lovecraftian, very detailed face, portrait"
 python scripts/stable_txt2img.py --prompt "Photo realism, an 8k, deep focused illustration of firstNameLastName person, moonshine, this person is beautiful, with tarot colors in the background, in the style of wes anderson with lot of love and cuteness created at antiquity in 4k ultra high resolution and with dutch angle, with inspiring feeling"
